5b99a1a21195896e5a4d6f2b6962b5679eb03e72
jim <guch23@gmail.com>
2019-09-27 14:34:51 -0500

    city selection and weather working

--- server/models/weather.js
+++ server/models/weather.js
@@ -5,7 +5,8 @@ const API_KEY = ('0d2a6b87ee54440835a2d85036498120');
 class Weather {
     static retrieveByCity (city, callback) {
         request({
-            uri: `https://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=${process.env.WEATHER_API_KEY}&units=imperial`,
+            //uri: `https://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=${process.env.WEATHER_API_KEY}&units=imperial`,
+            uri: `https://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=0d2a6b87ee54440835a2d85036498120&units=imperial`,
             json: true
         }).then((res) => {
             callback(res);
